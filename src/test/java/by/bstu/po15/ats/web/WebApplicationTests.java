4422a0ebbfd9f9819f4e02aa6abdf6a52d399133
Nastiats <nastiats07@gmail.com>
2026-04-12 16:59:00 +0300

    Создание проекта Spring boot

---
+++ src/test/java/by/bstu/po15/ats/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package by.bstu.po15.ats.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
