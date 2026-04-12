15c2bfc247fc447c5d99df5be815400bdb07e000
Nastiats <nastiats07@gmail.com>
2026-04-12 17:01:25 +0300

    Создание проекта Spring boot

---
+++ src/main/java/by/bstu/po15/ats/WebApplication.java
@@ -0,0 +1,11 @@
+package by.bstu.po15.ats.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication
+{	public static void main(String[] args)
+	{	SpringApplication.run(WebApplication.class, args);
+ 	}
+}
